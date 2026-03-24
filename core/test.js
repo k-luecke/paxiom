47a7f739b461fed6c2a905a757a31cff8cdee00e
k-luecke <k2luecke@gmail.com>
2026-03-24 12:25:56 -0400

    feat: add deterministic reducer for account credit and debit

--- core/test.js
+++ core/test.js
@@ -1,12 +1,36 @@
 const { createGenesisState } = require("./state");
 const { hashState } = require("./hash");
+const { applyAction } = require("./reducer");
 
-const state1 = createGenesisState();
-const state2 = createGenesisState();
+const genesis = createGenesisState();
+const genesisHash = hashState(genesis);
 
-const hash1 = hashState(state1);
-const hash2 = hashState(state2);
+const creditAction = {
+  action_id: "a1",
+  sequence: 1,
+  type: "ACCOUNT_CREDIT",
+  payload: {
+    account_id: "acct_1",
+    asset_key: "ethereum:usdc",
+    amount: "1000"
+  }
+};
 
-console.log("Hash 1:", hash1);
-console.log("Hash 2:", hash2);
-console.log("Equal:", hash1 === hash2);
+const debitAction = {
+  action_id: "a2",
+  sequence: 2,
+  type: "ACCOUNT_DEBIT",
+  payload: {
+    account_id: "acct_1",
+    asset_key: "ethereum:usdc",
+    amount: "250"
+  }
+};
+
+const state1 = applyAction(genesis, creditAction);
+const state2 = applyAction(state1, debitAction);
+
+console.log("Genesis hash:", genesisHash);
+console.log("State 1 hash:", hashState(state1));
+console.log("State 2 hash:", hashState(state2));
+console.log("Final balance:", state2.accounts["acct_1"].balances["ethereum:usdc"].available);
